eab9579c9b728d7a821e37702738249e5fe39222
ofir.oron <ofir.oron@toluna.com>
2022-02-03 14:45:16 +0200

    updated tools and icons, app steps, map of tools for each app step

--- src/constants.js
+++ src/constants.js
@@ -4,12 +4,20 @@ import {
     faAngleLeft,
     faAngleRight,
     faAngleUp,
-    faExclamationCircle, faFan,
-    faSun
+    faExclamationCircle,
+    faSatelliteDish,
+    faSolarPanel,
+    faSnowflake,
+    faTimes
 } from '@fortawesome/free-solid-svg-icons'
 import LevelIcon from "./components/LevelIcon";
 
+export const appSteps = {
+    OBSTACLES: '/obstacles',
+    BLOCKS: '/blocks',
+};
 
+export const ICON_SIZE = 16;
 
 export const gridBlockTypes = {
     LEVEL_A: {
@@ -24,53 +32,120 @@ export const gridBlockTypes = {
         color: '#00caff',
         icon: ({blockSize, text}) => <LevelIcon bgColor="#00caff" blockSize={blockSize}>{text}</LevelIcon>,
     },
-    SHAPE_DIRECTION_LEFT: {
-        id: 'shape-direction-left',
-        name: 'Shape Left',
+    LEVEL_C: {
+        id: 'level-c',
+        name: 'Level C',
+        color: '#00ce85',
+        icon: ({blockSize, text}) => <LevelIcon bgColor="#00ce85" blockSize={blockSize}>{text}</LevelIcon>,
+    },
+    LEVEL_D: {
+        id: 'level-d',
+        name: 'Level D',
+        color: '#ffda35',
+        icon: ({blockSize, text}) => <LevelIcon bgColor="#ffda35" blockSize={blockSize}>{text}</LevelIcon>,
+    },
+    SHAPE_DIRECTION_WEST: {
+        id: 'shape-direction-west',
+        name: 'Shape West',
         color: '',
         icon: <FontAwesomeIcon icon={faAngleLeft} />,
 
     },
-    SHAPE_DIRECTION_RIGHT: {
-        id: 'shape-direction-right',
-        name: 'Shape Right',
+    SHAPE_DIRECTION_EAST: {
+        id: 'shape-direction-east',
+        name: 'Shape East',
         color: '',
         icon: <FontAwesomeIcon icon={faAngleRight} />,
     },
-
-    SHAPE_DIRECTION_TOP: {
-        id:  'shape-direction-top',
-        name: 'Shape Top',
+    SHAPE_DIRECTION_NORTH: {
+        id:  'shape-direction-north',
+        name: 'Shape North',
         color: '',
         icon: <FontAwesomeIcon icon={faAngleUp} />,
     },
-
-    SHAPE_DIRECTION_BOTTOM: {
-        id:  'shape-direction-bottom',
-        name: 'Shape Bottom',
+    SHAPE_DIRECTION_SOUTH: {
+        id:  'shape-direction-south',
+        name: 'Shape South',
         color: '',
         icon: <FontAwesomeIcon icon={faAngleDown} />,
     },
-
-    OBSTACLE_BOILER: {
-        id:  'obstacle-boiler',
-        name: 'Boiler Obstacle',
+    BLOCKED: {
+        id:  'blocked',
+        name: 'Blocked',
         color: '',
-        icon: <FontAwesomeIcon icon={faSun} />,
+        icon: <FontAwesomeIcon icon={faTimes} />,
     },
-
-    OBSTACLE_AC: {
-        id:  'obstacle-ac',
-        name: 'A/C Obstacle',
+    OBSTACLE_BOILER_S: {
+        id:  'obstacle-boiler-s',
+        name: 'Small Boiler',
         color: '',
-        icon: <FontAwesomeIcon icon={faFan} />,
+        icon: <FontAwesomeIcon size="xs" icon={faSolarPanel} />,
     },
-
-    OBSTACLE_OTHER:{
+    OBSTACLE_BOILER_M: {
+        id:  'obstacle-boiler-m',
+        name: 'Medium Boiler',
+        color: '',
+        icon: <FontAwesomeIcon size="sm" icon={faSolarPanel} />,
+    },
+    OBSTACLE_BOILER_L: {
+        id:  'obstacle-boiler-l',
+        name: 'Large Boiler',
+        color: '',
+        icon: <FontAwesomeIcon size="lg" icon={faSolarPanel} />,
+    },
+    OBSTACLE_AC_S: {
+        id:  'obstacle-ac-s',
+        name: 'Small A/C',
+        color: '',
+        icon: <FontAwesomeIcon size="xs" icon={faSnowflake} />,
+    },
+    OBSTACLE_AC_M: {
+        id:  'obstacle-ac-m',
+        name: 'Medium A/C',
+        color: '',
+        icon: <FontAwesomeIcon size="sm" icon={faSnowflake} />,
+    },
+    OBSTACLE_AC_L: {
+        id:  'obstacle-ac-l',
+        name: 'Large A/C',
+        color: '',
+        icon: <FontAwesomeIcon size="lg" icon={faSnowflake} />,
+    },
+    OBSTACLE_ANTENNA: {
+        id:  'obstacle-antenna',
+        name: 'Antenna',
+        color: '',
+        icon: <FontAwesomeIcon icon={faSatelliteDish} />,
+    },
+    OBSTACLE_OTHER: {
         id:  'obstacle-other',
         name: 'Other Obstacle',
         color: '',
         icon: <FontAwesomeIcon icon={faExclamationCircle} />,
     },
+}
+
+export const toolbarItemsMap = {
+    [appSteps.OBSTACLES]: [
+        gridBlockTypes.OBSTACLE_OTHER,
+        gridBlockTypes.OBSTACLE_AC_S,
+        gridBlockTypes.OBSTACLE_AC_M,
+        gridBlockTypes.OBSTACLE_AC_L,
+        gridBlockTypes.OBSTACLE_BOILER_S,
+        gridBlockTypes.OBSTACLE_BOILER_M,
+        gridBlockTypes.OBSTACLE_BOILER_L,
+        gridBlockTypes.OBSTACLE_ANTENNA,
+    ],
+    [appSteps.BLOCKS]: [
+        gridBlockTypes.LEVEL_A,
+        gridBlockTypes.LEVEL_B,
+        gridBlockTypes.LEVEL_C,
+        gridBlockTypes.LEVEL_D,
+        gridBlockTypes.SHAPE_DIRECTION_EAST,
+        gridBlockTypes.SHAPE_DIRECTION_WEST,
+        gridBlockTypes.SHAPE_DIRECTION_NORTH,
+        gridBlockTypes.SHAPE_DIRECTION_SOUTH,
+        gridBlockTypes.BLOCKED,
+    ]
 
 }
